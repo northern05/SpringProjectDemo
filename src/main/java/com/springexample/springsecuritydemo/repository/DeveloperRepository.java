d06e6824b21411032000e3c172982133fe94e3b6
nothern05 <gleb5shimko@gmail.com>
2021-06-30 10:02:52 +0300

    Updating project. Adding Optional methods to repository

--- src/main/java/com/springexample/springsecuritydemo/repository/DeveloperRepository.java
+++ src/main/java/com/springexample/springsecuritydemo/repository/DeveloperRepository.java
@@ -3,6 +3,9 @@ package com.springexample.springsecuritydemo.repository;
 import com.springexample.springsecuritydemo.model.entity.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
     Developer findByEmail(String email);
+    Optional<Developer> getDeveloperById (Long id);
 }
--- src/main/java/com/springexample/springsecuritydemo/repository/ProjectRepository.java
+++ src/main/java/com/springexample/springsecuritydemo/repository/ProjectRepository.java
@@ -3,6 +3,9 @@ package com.springexample.springsecuritydemo.repository;
 import com.springexample.springsecuritydemo.model.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Project findProjectByName(String name);
+    Optional<Project> getProjectById (Long id);
 }
