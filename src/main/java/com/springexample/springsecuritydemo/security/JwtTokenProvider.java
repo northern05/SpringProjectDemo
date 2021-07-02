a225bc963b4fd0eb0a26a91384b3dea4d979bbdf
nothern05 <gleb5shimko@gmail.com>
2021-07-02 13:19:10 +0300

    Correcting mistakes

--- src/main/java/com/springexample/springsecuritydemo/security/JwtTokenProvider.java
+++ src/main/java/com/springexample/springsecuritydemo/security/JwtTokenProvider.java
@@ -61,7 +61,7 @@ public class JwtTokenProvider {
     }
 
     public UsernamePasswordAuthenticationToken getAuthentication(String token){
-        UserDetails userDetails = this.userDetailsService.loadUserByUsername(token);
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsername(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
